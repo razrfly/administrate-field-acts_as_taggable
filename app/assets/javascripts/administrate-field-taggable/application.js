eb4653502cfddef04997f5f814ed99cb7e6c1d9b
Holden Thomas <holden@razrfly.com>
2021-11-12 13:06:50 +0100

    test the waters

--- app/assets/javascripts/administrate-field-taggable/application.js
+++ app/assets/javascripts/administrate-field-taggable/application.js
@@ -1,19 +1,23 @@
 // ActsAsTaggableOn Administrate Form
-$(function() {
-  $(".field-unit--acts-as-taggable .field-unit__field > input").each(function(elem) {
-    var $this = $(this);
-    var opts = $this.data('tag-options');
+//$(function() {
+//  $(".field-unit--acts-as-taggable .field-unit__field > input").each(function(elem) {
+//    var $this = $(this);
+//    var opts = $this.data('tag-options');
+//
+//    $this.selectize({
+//      delimiter: ", ",
+//      persist: false,
+//      options: opts,
+//      create: function(input) {
+//        return {
+//          value: input,
+//          text: input
+//        };
+//      }
+//    });
+//  })
+//});
 
-    $this.selectize({
-      delimiter: ", ",
-      persist: false,
-      options: opts,
-      create: function(input) {
-        return {
-          value: input,
-          text: input
-        };
-      }
-    });
-  })
+$(function() {
+  $(".field-unit--acts-as-taggable .field-unit__field > input").selectize({});
 });
