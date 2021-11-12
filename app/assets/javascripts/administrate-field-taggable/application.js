bc7bac9e254b3572a58fdcd9ca389f6c93208190
Holden Thomas <holden@razrfly.com>
2021-11-12 13:17:13 +0100

    put it back

--- app/assets/javascripts/administrate-field-taggable/application.js
+++ app/assets/javascripts/administrate-field-taggable/application.js
@@ -1,23 +1,19 @@
 // ActsAsTaggableOn Administrate Form
-//$(function() {
-//  $(".field-unit--acts-as-taggable .field-unit__field > input").each(function(elem) {
-//    var $this = $(this);
-//    var opts = $this.data('tag-options');
-//
-//    $this.selectize({
-//      delimiter: ", ",
-//      persist: false,
-//      options: opts,
-//      create: function(input) {
-//        return {
-//          value: input,
-//          text: input
-//        };
-//      }
-//    });
-//  })
-//});
-
 $(function() {
-  $(".field-unit--acts-as-taggable .field-unit__field > input").selectize({});
+  $(".field-unit--acts-as-taggable .field-unit__field > input").each(function(elem) {
+    var $this = $(this);
+    var opts = $this.data('tag-options');
+
+    $this.selectize({
+      delimiter: ", ",
+      persist: false,
+      options: opts,
+      create: function(input) {
+        return {
+          value: input,
+          text: input
+        };
+      }
+    });
+  })
 });
